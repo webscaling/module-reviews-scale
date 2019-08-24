b009c664c56a48ce1059acdb2dc2fa49d3884613
Arohan Dutt <arohandutt@gmail.com>
2019-08-23 19:09:42 -0500

    Test to verify git branch connection

--- src/components/ReviewSummary/ReviewSummary.jsx
+++ src/components/ReviewSummary/ReviewSummary.jsx
@@ -6,7 +6,7 @@ const ReviewSummary = (props) => {
       <h1 class='rev_rev_container_header'>22 Customer Reviews</h1>
       <div id='rev_star_container'>
         <i id='rev_overall_stars'></i>
-        <div>3.9 out of 5 stars</div>
+        <div>3.8 out of 5 stars</div>
       </div>
       <table id='rev_breakdown_table'>
         <tr>
