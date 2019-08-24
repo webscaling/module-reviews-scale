9ed469a0603ba88fd5c2c79ba0653cbc222f2863
Arohan Dutt <arohandutt@gmail.com>
2019-08-23 20:34:21 -0500

    Wireframe for reviews section

---
+++ src/components/ReviewContainer/Review.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Review = (props) => {
+  return(
+    <div id='rev_review'>
+
+    </div>
+  )
+}
+
+export default Review
---
+++ src/components/ReviewContainer/ReviewContainer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Review from './Review.jsx'
+
+const ReviewContainer = (props) => {
+  return (
+    <div id='rev_reviews_container'>
+      <span>Showing 1 - 8 of 12 reviews</span>
+      <select id='rev_sort'>
+        <option value='top_reviews'>Top Reviews</option>
+        <option value='most_recent'>Most Recent</option>
+      </select>
+      <Review />
+      <Review />
+      <Review />
+      <Review />
+    </div>
+  )
+}
+
+export default ReviewContainer
