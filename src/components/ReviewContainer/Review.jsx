ef407b614d8de00a2b9f7627c474616ca5dc5d93
Arohan Dutt <arohandutt@gmail.com>
2019-08-26 17:27:06 -0500

    Add helpful count

--- src/components/ReviewContainer/Review.jsx
+++ src/components/ReviewContainer/Review.jsx
@@ -8,7 +8,7 @@ const Review = (props) => {
     <div id='rev_review'>
       <div className={'rev_pic_title'}>
         <img className={'rev_avatar'} src={props.reviewData.avatarURL} />
-        <span className={'rev_author'}>Elmo Street</span>
+        <span className={'rev_author'}>{props.reviewData.author}</span>
       </div>
       <div className={'rev_rev_rating_stars'}>
         <div className={'rev_rev_stars'}>
@@ -27,6 +27,7 @@ const Review = (props) => {
       </div>
       <span className={'rev_rev_date'}>July 7th, 2019</span>
       <span className={'rev_rev_text'}>{props.reviewData.text}</span>
+      <span className={'rev_rev_helpfulCount'}>{`${props.reviewData.helpfulCount} people found this helpful`}</span>
 
     </div>
   )
