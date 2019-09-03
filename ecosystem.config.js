9ffa9663966d02ee76577f1b7a034d8613658d2f
Arohan Dutt <arohandutt@gmail.com>
2019-09-03 13:00:56 -0500

    even more pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,7 @@ module.exports = {
       key: '~/.ssh/hratx43-FEC-Reviews-Module.pem',
       ref: 'origin/master',
       repo: 'git@github.com:shazamazon/module-reviews.git',
+      path: '/home/ubuntu/module-reviews/server/server.js',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
